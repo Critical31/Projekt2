91d804ab68d16aac578c21d5100f0fc05bc4ffcc
Critical <Critical@DESKTOP-QTN5GT6>
2022-10-11 19:31:55 +0200

    test

---
+++ kred_01/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,85 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String kwota;
+	private String okres;
+	private String procent;
+	private Double rata;
+
+	@Inject
+	FacesContext ctx;
+
+	
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOkres() {
+		return okres;
+	}
+
+	public void setOkres(String okres) {
+		this.okres = okres;
+	}
+
+	public String getProcent() {
+		return procent;
+	}
+
+	public void setProcent(String procent) {
+		this.procent = procent;
+	}
+
+	public Double getRata() {
+		return rata;
+	}
+
+	public void setRata(Double rata) {
+		this.rata = rata;
+	}
+
+	public boolean obliczRate() {
+		try {
+			double x = Double.parseDouble(this.kwota);
+			double y = Double.parseDouble(this.okres);
+			double z = Double.parseDouble(this.procent);
+			
+			double w = ((x + (x * z))/y);
+
+			rata = (double) (Math.round(w*100.0)/100.0);
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String oblicz() {
+		if (obliczRate()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+
+	
+}
